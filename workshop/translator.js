c6df5b42cd53fbb59a1b61b5cc82b65fefbea1fb
Phil Nash <philnash@gmail.com>
2025-11-26 15:26:02 +1030

    Translation works

--- workshop/translator.js
+++ workshop/translator.js
@@ -1,22 +1,59 @@
-async function init() {
-  const posts = document.querySelectorAll("article");
+const errorMessage = "LanguageDetector is not available in this environment.";
 
+async function createLanguageDetector() {
   if ("LanguageDetector" in window) {
     const detectorAvailability = await LanguageDetector.availability();
     if (detectorAvailability === "unavailable") {
-      console.log("Language detection is unavailable in this environment.");
-      return;
+      throw new Error(errorMessage);
     }
     const detector = await LanguageDetector.create();
+    return detector;
+  } else {
+    throw new Error(errorMessage);
+  }
+}
 
-    const homeLanguage = navigator.languages.find((lang) => lang.length === 2);
-    console.log(homeLanguage);
+async function detectLanguage(text, detector) {
+  const detectedLanguages = await detector.detect(text);
+  return detectedLanguages[0].detectedLanguage;
+}
+
+async function translationAvailable(sourceLanguage, targetLanguage) {
+  console.log({ sourceLanguage, targetLanguage });
+  const translatable = await Translator.availability({
+    sourceLanguage: sourceLanguage,
+    targetLanguage: targetLanguage,
+  });
+  console.log(translatable === "unavailable");
+  if (translatable === "unavailable") {
+    throw new Error(
+      `Translation not available for ${sourceLanguage} to ${targetLanguage}`
+    );
+  }
+  return true;
+}
+
+async function translate(text, sourceLanguage, targetLanguage) {
+  const translator = await Translator.create({
+    sourceLanguage,
+    targetLanguage,
+  });
+  return await translator.translate(text);
+}
+
+async function init() {
+  const posts = document.querySelectorAll("article");
+
+  try {
+    const homeLanguage = navigator.languages[0].split("-")[0];
+
+    const detector = await createLanguageDetector();
 
     posts.forEach((post) => {
       setupPost(post, detector, homeLanguage);
     });
-  } else {
-    console.log("LanguageDetector is not available in this environment.");
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -25,22 +62,39 @@ async function setupPost(post, detector, homeLanguage) {
   const actions = post.querySelector(".actions");
   const text = content.textContent;
 
-  const detectedLanguage = (await detector.detect(text))[0].detectedLanguage;
+  const detectedLanguage = await detectLanguage(text, detector);
 
-  const translatable = await Translator.availability({
-    sourceLanguage: detectedLanguage,
-    targetLanguage: homeLanguage,
-  });
-  console.log(translatable);
+  const translatable = await translationAvailable(
+    detectedLanguage,
+    homeLanguage
+  );
 
-  const translateButton = document.createElement("button");
-  translateButton.className = "translate-button";
-  translateButton.textContent = "Translate";
-  translateButton.addEventListener("click", () => {
-    // Placeholder for translation logic
-    alert(`Translating post: ${text}`);
-  });
-  actions.appendChild(translateButton);
+  if (translatable) {
+    const translateButton = document.createElement("button");
+    translateButton.className = "translate-button";
+    translateButton.textContent = "Translate";
+    translateButton.addEventListener("click", async () => {
+      translateButton.classList.add("loading");
+
+      const newText = await translate(text, detectedLanguage, homeLanguage);
+
+      const newP = createContent(newText);
+      content.insertAdjacentElement("afterend", newP);
+      translateButton.remove();
+    });
+    actions.appendChild(translateButton);
+  } else {
+    console.log(
+      `Translation not available for ${detectedLanguage} to ${homeLanguage}`
+    );
+  }
+}
+
+function createContent(text) {
+  const content = document.createElement("p");
+  content.className = "content";
+  content.textContent = text;
+  return content;
 }
 
 if (document.readyState === "loading") {
